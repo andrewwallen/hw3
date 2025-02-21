cca2c0b1ca74d9f76899f615d846a986f5360124
Andrew Wallen <andrew.wallen@kellogg.northwestern.edu>
2025-02-21 22:18:15 +0000

    made updates to entries pages

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,34 @@
 class EntriesController < ApplicationController
+
+  def show
+    @entry = Entry.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" => @entry["place_id"]})
+    # render contacts/show view with details about Contact
+  end
+
+  def index
+    @place = Place.find_by({"id" => params["place_id"]})
+    @entries = Entry.where({place_id => @place["id"]})
+  end
+  
+  def new
+  end
+
+  def create
+    @entry = Entry.new
+
+  
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+
+   
+    @entry.save
+
+    # redirect user
+    redirect_to "/places/#{@entry["place_id"]}/entries"
+  end
+
 end
+
